069bcd358ce2c3f7be4d5f13a3864f9a665fa374
Ted Won <jwon@redhat.com>
2021-05-02 02:23:28 +1000

    Added List<PathSegment> usage code and log for the error

--- src/main/java/org/acme/getting/started/ReactiveGreetingResource.java
+++ src/main/java/org/acme/getting/started/ReactiveGreetingResource.java
@@ -1,16 +1,62 @@
 package org.acme.getting.started;
 
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import org.jboss.resteasy.reactive.RestSseElementType;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.PathSegment;
+import java.util.List;
 
 @Path("/hello")
 public class ReactiveGreetingResource {
 
+    @Inject
+    ReactiveGreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public Uni<String> greeting(String name) {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{count}/{name}")
+    public Multi<String> greetings(int count, String name) {
+        return service.greetings(count, name);
+    }
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestSseElementType(MediaType.TEXT_PLAIN)
+    @Path("/stream/{count}/{name}")
+    public Multi<String> greetingsAsStream(int count, String name) {
+        return service.greetings(count, name);
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy Reactive";
+        return "hello";
+    }
+
+    // https://docs.jboss.org/resteasy/docs/4.6.0.Final/userguide/html_single/index.html#_PathParam_and_PathSegment
+    @GET
+    @Path("/book/{id}")
+    public String getBook(@PathParam("id") PathSegment id) {
+        return id.getPath();
+    }
+
+    @GET
+    @Path("/jwon/{keyword:.*}")
+    public String searchByKeywords(@PathParam("keyword") List<PathSegment> keywords) {
+        return keywords.toString();
     }
 }
